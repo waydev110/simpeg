88880cb055ce63fa3dab4ed8117ed3ab6110bcf2
wayseptian <wahyu.septian@greenlabgroup.com>
2019-08-23 07:48:46 +0700

    Perubahan File CobaController

--- app/Http/Controllers/CobaController.php
+++ app/Http/Controllers/CobaController.php
@@ -15,7 +15,7 @@ class CobaController extends Controller
     public function index()
     {
         
-        $data = DB::table('AA')->get();
+        $data = DB::table('OK')->get();
         dump($data);
     }
 
